d87f0daae2c2b7ad0c6a7b2a4cdbb41ace741ccc
t.shibuta <make.app.ts@gmail.com>
2017-02-23 20:57:15 +0900

    postcss-smart-import から postcss-import に変更

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    require('postcss-smart-import')({}),
+    require('postcss-import')({}),
     require('precss')({}),
     require('autoprefixer')({
       browsers: ['IE 9', 'IE 10', 'IE 11', 'last 2 versions']
